30ca187155ba2cf15ef7c4d16ff7fe286b1f5a9e
Владислав Микитчук <Mykytchuk_Vl@ifnmu.edu.ua>
2024-10-29 14:30:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public delegate void OperationDelegate(int x, int y);
+    public static void Add(int x, int y)
+    {
+        Console.WriteLine("Додавання: " + (x + y));
+    }
+    public static void Multiply(int x, int y)
+    {
+        Console.WriteLine("Множення: " + (x * y));
+    }
+
+    public static void Main()
+    {
+        OperationDelegate addDelegate = Add;
+        OperationDelegate multiplyDelegate = Multiply;
+        OperationDelegate combinedDelegate = addDelegate + multiplyDelegate;
+
+        int a = 5;
+        int b = 3;
+        combinedDelegate(a, b);
+    }
+}
+
